2f7ceb08e653dc5fed0360997c52c3b8b3823aea
Kevin Munoz <kevinmunoz0421@gmail.com>
2022-08-09 23:04:32 -0700

    Complete library object isRead toggle and removing instance from array

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 'use strict';
 
+let BOOKS = new Array(); 
+
+window.addEventListener('load', function()
+{
+    removeBookFunctionality();
+    changeToggleFunctionality()
+})
+
 let addBookButton = document.getElementById("cta-button");
 let addBookFormButton = document.getElementById("form-submit-button");
 let bookModal = document.getElementsByClassName("form-modal-outer")[0];
@@ -42,17 +50,35 @@ Book.prototype.addBook = function()
 {
     let newCard = document.createElement('div');
     newCard.classList.add('card-container');
-    
-    newCard.innerHTML = 
+    newCard.setAttribute('data-index-number', `${BOOKS.length}`);
+
+    if(this.isRead === true)
+    {
+        newCard.innerHTML = 
         `<p class="book-name">${this.title}</p>
         <p class="book-author">${this.author}</p>
         <p class="book-pages-container"><span class="book-pages">${this.pages}</span> pages</p>
         <label for="read-status">Read</label>
         <label class="toggle">
                 <input type="checkbox" id="read-status">
-                <span class="toggle-control"></span>
+                <span class="toggle-control read-true"></span>
         </label>
         <button class="cta-secondary">Remove</button>`;
+    }
+    else if(this.isRead === false)
+    {
+        newCard.innerHTML = 
+        `<p class="book-name">${this.title}</p>
+        <p class="book-author">${this.author}</p>
+        <p class="book-pages-container"><span class="book-pages">${this.pages}</span> pages</p>
+        <label for="read-status">Read</label>
+        <label class="toggle">
+                <input type="checkbox" id="read-status">
+                <span class="toggle-control read-false"></span>
+        </label>
+        <button class="cta-secondary">Remove</button>`;
+
+    }
 
     mainHTML.appendChild(newCard);
 
@@ -71,4 +97,66 @@ newBookForm.addEventListener('submit', function()
     console.log(newBook);
 
     newBook.addBook();
+    BOOKS.push(newBook);
+    console.log(BOOKS);
+    removeBookFunctionality();
+    changeToggleFunctionality();
 })
+
+
+
+function removeBookFunctionality()
+{
+    let removeBookHTML = document.querySelectorAll(".cta-secondary");
+
+    for(let i = 0; i < removeBookHTML.length; i ++)
+    {
+        removeBookHTML[i].addEventListener('click', function(e)
+        {
+            let cardContainerHMTL = e.target.parentElement;
+
+            let indexNumber = cardContainerHMTL.getAttribute('data-index-number');
+
+            BOOKS[indexNumber] = undefined;
+
+            console.log(BOOKS);
+
+            cardContainerHMTL.remove();
+        })
+    }
+    
+}
+
+
+
+function changeToggleFunctionality()
+{
+    let toggleHTML = document.querySelectorAll(".toggle-control");
+
+    for(let i = 0; i < toggleHTML.length; i ++)
+    {
+        toggleHTML[i].addEventListener('click', function(e)
+        {
+            if(e.target.classList.contains('read-true'))
+            {
+                e.target.classList.remove('read-true');
+                e.target.classList.add('read-false');
+
+                BOOKS[e.target.closest('.card-container').getAttribute('data-index-number')].isRead = false;
+
+                console.log(BOOKS[e.target.closest('.card-container').getAttribute('data-index-number')]);
+            }
+            else if(e.target.classList.contains('read-false'))
+            {
+                e.target.classList.remove('read-false');
+                e.target.classList.add('read-true');
+
+                BOOKS[e.target.closest('.card-container').getAttribute('data-index-number')].isRead = true;
+
+                console.log(BOOKS[e.target.closest('.card-container').getAttribute('data-index-number')]);
+            }
+        })
+    }
+
+    
+}
